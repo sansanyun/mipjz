609764a633f9cbecbecb1e1cbba3c88ff5f74233
sansanyun <85249417@qq.com>
2019-01-01 18:03:50 +0800

    no message

--- app/search/controller/Search.php
+++ app/search/controller/Search.php
@@ -5,15 +5,19 @@ namespace app\search\controller;
 use think\Request;
 use think\Response;
 use app\common\lib\Htmlp;
+use app\common\lib\Paginations;
 use app\common\controller\Base;
 class Search extends Base
 {
     public function index()
     {
         $keywords = Htmlp::htmlp(trim(input('param.q')," \t\n\r\0\x0B"));
+        $page = Htmlp::htmlp(trim(input('param.page')," \t\n\r\0\x0B"));
+        $page = $page ? $page : 1;
         if (!$keywords) {
             return $this->error('请输入搜索的关键词','');
         }
+        $limit = 10;
         $postData = array(
             "data"  => $keywords,
             'respond' => 'json',
@@ -31,6 +35,7 @@ class Search extends Base
                         $likeWhere[] = ['like','%'.$tempKeywords.'%'];
                     }
                     $tempList = db('Articles')->whereOr('title',$likeWhere)->select();
+                    $count = db('Articles')->whereOr('title',$likeWhere)->count();
                     if ($tempList) {
                         $tempArray = [];
                         foreach ($tempList as $key => $value) {
@@ -53,11 +58,17 @@ class Search extends Base
                 }
             }
         }
+
         $this->assign('keywords',$keywords);
         if ($tempIdsArray) {
-            foreach ($tempIdsArray as $key => $value) {
-                if ($key < 20) {
-                    $itemList[] = model('app\article\model\Articles')->getItemInfo('',$value);
+            $allPageNum = ceil(count($tempIdsArray) / $limit);
+            if ($page <= $allPageNum) {
+                $newIds = array_slice($tempIdsArray,($page - 1) * $limit,$limit);
+                if ($newIds) {
+                    foreach ($newIds as $uuid) {
+                        $itemList[] =  model('app\article\model\Articles')->getItemInfo('',$uuid);
+                    }
+                    $itemCount = count($tempIdsArray);
                 }
             }
             $this->assign('itemList',$itemList);
@@ -65,11 +76,23 @@ class Search extends Base
             $this->assign('itemList','');
         }
       
+        $pageText = ($page == 1) ? "" : $this->siteInfo['titleSeparator'] . "第" . $page . "页";
         //标题关键词描述
-        $mipTitle = $keywords . $this->siteInfo['titleSeparator'] . $this->siteInfo['siteName'];
+        $mipTitle = $keywords . $pageText . $this->siteInfo['titleSeparator'] . $this->siteInfo['siteName'];
         $this->assign('mipTitle', $mipTitle);
         $this->assign('mipKeywords',$keywords);
         $this->assign('mipDescription',$keywords);
+        
+        $baseUrl = $this->domain . '/search?q=' . $keywords;
+        $pagination_array = array(
+            'base_url' => $baseUrl,
+            'total_rows' => $itemCount,
+            'per_page' => $limit,
+            'page_break' => '='
+        );
+        $pagination = new Paginations($pagination_array);
+        $pagination = $pagination->create_links();
+        $this->assign('pagination',$pagination);
           
         return $this->mipView('search/search');
     }
